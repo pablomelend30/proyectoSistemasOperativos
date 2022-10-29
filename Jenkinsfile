b5a61ce8aae279db49ef0405ef66776e8cecbdc2
pablomelend30 <pmelendezm@miumg.edu.gt>
2022-10-28 20:47:37 -0600

    asfasrta3245234152345t

--- Jenkinsfile
+++ Jenkinsfile
@@ -14,9 +14,9 @@ pipeline {
                 sh 'npm install'
             }
         }
-
         stage('Deliver') {
             steps {
+                sh "chmod +x -R ${env.WORKSPACE}"
                 sh './jenkins/scripts/deliver.sh'
                 input message: 'Finished using the web site? (Click "Proceed" to continue)?'
                 sh './jenkins/scripts/kill.sh'
--- src/cliente/js/index.js
+++ src/cliente/js/index.js
@@ -5,28 +5,5 @@ require('../css/style.css');
 
 AOS.init();
 
-document.getElementById("idBtnEnviar").addEventListener("click",function(){
-    let strCorreo = document.getElementById("idInCE").value;
-    let strNombre = document.getElementById("idInNom").value;
-    let strMensaje = document.getElementById("idInMen").value;
 
-    if(strCorreo != "" && strNombre != "" && strMensaje != ""){
-        let datos ={
-            c: strCorreo,
-            n: strNombre,
-            m:strMensaje
-        };
-        axios.post('/api/contacto',datos)
-        .then(function(response){
-            document.getElementById("idInCE").value="";
-            document.getElementById("idInNom").value="";
-            document.getElementById("idInMen").value="";
-            alert("Gracias por escribirnos, en breve te contactaremos");
-        }).catch(function(error){
-            console.log(error);
-        });
-    }else{
-        alert("Por fovor rellenar todos los campos");
-    }
 
-});
